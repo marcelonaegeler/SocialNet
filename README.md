f2db0bcb77b359904f206532b4577086272d79d3
Marcelo André Naegeler <marcelo.andre.naegeler@gmail.com>
2014-04-17 13:48:19 -0300

    Register controller and template

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ SocialNet
 =========
 
 This is the application learned in the book "Building Node applications with MongoDB and Backbone"
+
+Book's current page: 63
--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.get('/account/authenticated', function(req, res){
 	}
 });
 
+app.post('/register', function(req, res) {
+	var firstName = req.param('firstName', '');
+	var lastName = req.param('lastName', '');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+
+	if(null == email || null == password) {
+		res.send(400);
+		return;
+	}
+
+	Account.register(email, password, firstName, lastName);
+	res.send(200);
+});
+
 app.listen(8080, function(){
 	console.log('Server running at http://localhost:8080');
 });
