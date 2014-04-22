68da5dbffdee8af29a0ea26ecd8518d3d9a8d421
Marcelo André Naegeler <marcelo.andre.naegeler@gmail.com>
2014-04-21 23:30:45 -0300

    Exercícios feitos até a página 98

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ SocialNet
 
 This is the application learned in the book "Building Node applications with MongoDB and Backbone"
 
-Book's current page: 63
+Book's current page: 98
--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+	, app = express()
+	, nodemailer = require('nodemailer')
+	, MemoryState = require('connect').session.MemoryState;
+
+// Import the data layer
+var mogoose = require('mongoose');
+var config = {
+	mail: require('./config/mail')
+}
+
+// Import the accounts
+var Account = require('./models/Account')(config, mongoose, nodemailer);
 
 app.configure(function(){
-	app.use(express.static(__dirname + '/public'));	
 	app.set('view engine', 'jade');
+	app.use(express.static(__dirname + '/public'));	
+	app.use(express.limit('1mb'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "SocialNet secret key", store: new MemoryState() }));
+	mongoose.connect('mogodb://localhost/nodebackbone');
 });
 
 app.get('/', function(req, res){
-	res.render('index', {layout: false});
+	res.render('index.jade', {layout: false});
 });
 
-app.get('/account/authenticated', function(req, res){
-	if(req.session.loggedIn) {
-		res.send(200);
-	} else {
-		res.send(401);
+app.post('/login', function(req, res) {
+	console.log('login request');
+	var email = req.param('email', null)
+		, password = req.param('password', null);
+
+	if(null == email || email.length < 1 || null == password || password.length < 1) {
+		res.send(400);
+		return;
 	}
+
+	Account.login(email, password, function(success) {
+		if(!success) {
+			res.send(401);
+			return;
+		}
+		console.log('login was successfull');
+		res.send(200);
+	});
+
 });
 
 app.post('/register', function(req, res) {
@@ -31,6 +60,56 @@ app.post('/register', function(req, res) {
 
 	Account.register(email, password, firstName, lastName);
 	res.send(200);
+});
+
+app.get('/account/authenticated', function(req, res){
+	if(req.session.loggedIn) {
+		res.send(200);
+	} else {
+		res.send(401);
+	}
+});
+
+app.post('/forgotpassword', function(req, res) {
+	var hostname = req.headers.host
+		, resetPasswordUrl = 'http://' + hostname + '/resetPassword'
+		, email = req.param('email', null);
+
+	if(null == email || email.length < 1) {
+		res.send(400);
+		return;
+	}
+
+	Account.forgotPassword(email, resetPassword, function(success) {
+		if(success) {
+			res.send(200);
+		} else {
+			// User name or password not found
+			res.send(404);
+		}
+	});
+});
+
+app.get('/resetPassword', function(req, res) {
+	var accountId = req.param('account', null);
+	res.render('resetPassword.jade', { locals:{ accountId: accountId } });
+});
+
+app.post('resetPassword', function(req, res) {
+	var accountId = req.param('accountId', null)
+		, password = req.param('password', null);
+	if(null != accountId && null != password) {
+		Account.changePassword(accountId, password);
+	}
+	res.render('resetPasswordSuccess.jade');
+});
+
+app.get('/accounts/:id', function(req, res) {
+	var accountId = req.params.id == 'me' ? req.session.accountId : req.params.id;
+
+	Account.findOne({ _id:accountId }, function(account) {
+		res.send(account);
+	});
 });
 
 app.listen(8080, function(){
